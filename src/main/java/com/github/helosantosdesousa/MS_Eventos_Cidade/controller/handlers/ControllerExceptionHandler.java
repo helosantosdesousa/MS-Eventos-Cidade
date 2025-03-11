252c6a421a97b947c9ac97e7474abcd570a48249
helosantosdesousa <ssheloly@gmail.com>
2025-03-11 20:15:43 -0300

    fim do slide 6

---
+++ src/main/java/com/github/helosantosdesousa/MS_Eventos_Cidade/controller/handlers/ControllerExceptionHandler.java
@@ -0,0 +1,37 @@
+package com.github.helosantosdesousa.MS_Eventos_Cidade.controller.handlers;
+
+import com.github.helosantosdesousa.MS_Eventos_Cidade.controller.handlers.dto.CustomErrorDTO;
+import com.github.helosantosdesousa.MS_Eventos_Cidade.controller.handlers.dto.FieldMessageDTO;
+import com.github.helosantosdesousa.MS_Eventos_Cidade.controller.handlers.dto.ValidationErrorDTO;
+import com.github.helosantosdesousa.MS_Eventos_Cidade.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.Instant;
+
+@RestControllerAdvice
+public class ControllerExceptionHandler {
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<CustomErrorDTO> handleResourceNotFound(ResourceNotFoundException e,
+                                                                 HttpServletRequest request) {
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        CustomErrorDTO err = new CustomErrorDTO(Instant.now(), status.value(), e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomErrorDTO> methodArgumentNotValidation(MethodArgumentNotValidException e,
+                                                                      HttpServletRequest request){
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationErrorDTO err = new ValidationErrorDTO(Instant.now(), status.value(), "Dados inválidos", request.getRequestURI());
+
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()){
+            err.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+        return ResponseEntity.status(status).body(err);
+    }
+}
---
+++ src/main/java/com/github/helosantosdesousa/MS_Eventos_Cidade/controller/handlers/dto/ValidationErrorDTO.java
@@ -0,0 +1,20 @@
+package com.github.helosantosdesousa.MS_Eventos_Cidade.controller.handlers.dto;
+
+import lombok.Getter;
+
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+public class ValidationErrorDTO extends CustomErrorDTO{
+    private List<FieldMessageDTO> errors = new ArrayList<>();
+    public ValidationErrorDTO(Instant timestamp, Integer status, String error, String path){
+        super(timestamp, status, error, path);
+    }
+
+    public void addError(String fieldName, String message){
+        errors.removeIf(x -> x.getFieldName().equals(fieldName));
+        errors.add(new FieldMessageDTO(fieldName, message));
+    }
+}
