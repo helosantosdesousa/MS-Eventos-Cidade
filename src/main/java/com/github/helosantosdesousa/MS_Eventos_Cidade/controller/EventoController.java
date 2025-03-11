3f1318b389981e3db81f50da70c4aac54946ae9a
helosantosdesousa <ssheloly@gmail.com>
2025-03-11 19:03:20 -0300

    pag 17 slide 4

--- src/main/java/com/github/helosantosdesousa/MS_Eventos_Cidade/controller/EventoController.java
+++ src/main/java/com/github/helosantosdesousa/MS_Eventos_Cidade/controller/EventoController.java
@@ -6,7 +6,9 @@ import com.github.helosantosdesousa.MS_Eventos_Cidade.service.EventoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -14,13 +16,39 @@ import java.util.List;
 
 public class EventoController {
 
-@Autowired
+    @Autowired
     private EventoService service;
 
-@GetMapping
-    public ResponseEntity<List<EventoResponseDTO>> findAll(){
-    List<EventoResponseDTO> dto = service.findAll();
+    @GetMapping
+    public ResponseEntity<List<EventoResponseDTO>> findAll() {
+        List<EventoResponseDTO> dto = service.findAll();
 
-    return ResponseEntity.ok(dto);
-}
+        return ResponseEntity.ok(dto);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<EventoResponseDTO> findById(@PathVariable Long id){
+        EventoResponseDTO dto = service.findById(id);
+        return ResponseEntity.ok(dto);
+    }
+
+    @PostMapping
+    public ResponseEntity<EventoResponseDTO> insert(@RequestBody EventoRequestDTO requestDTO){
+        EventoResponseDTO dto = service.insert(requestDTO);
+
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequestUri()
+                .path("/{id}")
+                .buildAndExpand(dto.id())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EventoResponseDTO> update(@PathVariable Long id, @RequestBody EventoRequestDTO requestDTO){
+        EventoResponseDTO dto = service.update(id,requestDTO);
+        return ResponseEntity.ok(dto);
+    }
+
 }
--- src/main/java/com/github/helosantosdesousa/MS_Eventos_Cidade/service/EventoService.java
+++ src/main/java/com/github/helosantosdesousa/MS_Eventos_Cidade/service/EventoService.java
@@ -1,8 +1,10 @@
 package com.github.helosantosdesousa.MS_Eventos_Cidade.service;
 
+import com.github.helosantosdesousa.MS_Eventos_Cidade.dto.EventoRequestDTO;
 import com.github.helosantosdesousa.MS_Eventos_Cidade.dto.EventoResponseDTO;
 import com.github.helosantosdesousa.MS_Eventos_Cidade.entities.Evento;
 import com.github.helosantosdesousa.MS_Eventos_Cidade.repositories.EventoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,4 +23,40 @@ public class EventoService {
 
         return list.stream().map(EventoResponseDTO::new).toList();
     }
+    @Transactional(readOnly = true)
+    public EventoResponseDTO findById(Long id){
+        Evento entity = repository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Recurso não encontrado. Id: " + id)
+        );
+        return new EventoResponseDTO(entity);
+    }
+
+    @Transactional
+    public EventoResponseDTO insert(EventoRequestDTO requestDTO){
+        Evento entity = new Evento();
+
+        toEntity(requestDTO, entity);
+        entity = repository.save(entity);
+        return new EventoResponseDTO(entity);
+    }
+
+    private void toEntity(EventoRequestDTO requestDTO, Evento entity){
+        entity.setNome(requestDTO.nome());
+        entity.setData(requestDTO.data());
+        entity.setUrl(requestDTO.url());
+    }
+
+    @Transactional EventoResponseDTO update(Long id, EventoRequestDTO requestDTO){
+        try{
+            Evento entity = repository.getReferenceById(id);
+            toEntity(requestDTO, entity);
+            entity = repository.save(entity);
+            return new EventoResponseDTO(entity);
+        } catch (EntityNotFoundException ex){
+            throw new EntityNotFoundException("Recurso não encontrado. Id: " + id);
+        }
+    }
+
+
+
 }
